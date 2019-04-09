5698de95e1e5b01fd7f9231784f170ed4235b5d0
Chad Kouse <chad.kouse@gmail.com>
2019-04-08 21:21:15 -0400

    adding missing scripts and fonts

---
+++ .zsh.after/powerlevel10k.zsh
@@ -0,0 +1,5 @@
+source /usr/share/zsh-theme-powerlevel10k/gitstatus/gitstatus.plugin.zsh
+source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+
+export DEFAULT_USER=chadkouse
+export POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_last

561379aea2c923356fac6b3d30e13831784c4fe1
Chad Kouse <chad.kouse@gmail.com>
2019-10-09 13:21:45 -0400

    Update lots of stuff

--- .zsh.after/powerlevel10k.zsh
+++ .zsh.after/powerlevel10k.zsh
@@ -1,5 +1,8 @@
+echo "HERE"
 source /usr/share/zsh-theme-powerlevel10k/gitstatus/gitstatus.plugin.zsh
 source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
 
 export DEFAULT_USER=chadkouse
-export POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_last
+export POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
+export POWERLEVEL9K_SHORTEN_DELIMITER=""
+export POWERLEVEL9K_SHORTEN_STRATEGY=truncate_from_right

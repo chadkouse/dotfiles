2d6e093834cd5b14add9e636afb94185c922f6e3
Chad Kouse <chad.kouse@gmail.com>
2019-10-16 16:41:04 -0400

    dwm startup scripts

---
+++ .dwm/autostart_blocking.sh
@@ -0,0 +1,4 @@
+compton -b --config /home/chadkouse/.config/compton.conf
+/home/chadkouse/.scripts/tools/setbg
+/home/chadkouse/.scripts/tools/enabledvsync
+/home/chadkouse/.scripts/tools/fix_mouse_scroll
